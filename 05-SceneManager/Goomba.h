9eb30623b4eb6f72f87aedfa4613a70128633901
Present111 <22521118@gm.uit.edu.vn>
2024-05-08 07:52:54 +0700

    Chỉnh ID gooba

--- 05-SceneManager/Goomba.h
+++ 05-SceneManager/Goomba.h
@@ -1,17 +1,34 @@
 #pragma once
 #include "GameObject.h"
 #include "AssetIDs.h"
+#define GOOMBA_GRAVITY 0.002f
+#define GOOMBA_WALKING_SPEED 0.05f
+
+
+#define GOOMBA_BBOX_WIDTH 16
+#define GOOMBA_BBOX_HEIGHT 16
+#define GOOMBA_BBOX_HEIGHT_DIE 10
+
+#define GOOMBA_DIE_TIMEOUT 500
+
+#define GOOMBA_STATE_WALKING 100
+#define GOOMBA_STATE_DIE 200
+
+#define ID_ANI_GOOMBA_WALKING 5000
+#define ID_ANI_GOOMBA_DIE 5001
+
+
 
 class CGoomba : public CGameObject
 {
 protected:
-	float ax;				
-	float ay; 
+	float ax;
+	float ay;
 
 	ULONGLONG die_start;
 
-	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
-	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render();
 
 	virtual int IsCollidable() { return 1; };
@@ -20,7 +37,7 @@ protected:
 
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
 
-public: 	
+public:
 	CGoomba(float x, float y);
 	virtual void SetState(int state);
 };
