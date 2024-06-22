4c65a201eece73fd1883b2efce17e2ee672c8816
Present111 <22521118@gm.uit.edu.vn>
2024-06-22 23:13:27 +0700

    fix lại logic world map

--- 05-SceneManager/ObjectInWorldmap.cpp
+++ 05-SceneManager/ObjectInWorldmap.cpp
@@ -1,1 +1,17 @@
 #include "ObjectInWorldMap.h"
+#include "Grass.h"
+
+void CObjectInWorldMap::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+	animations->Get(ID_ANI_GRASS)->Render(x, y);
+	//RenderBoundingBox();
+}
+
+void CObjectInWorldMap::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - OBJECT_BBOX_WIDTH / 2;
+	t = y - OBJECT_BBOX_HEIGHT / 2;
+	r = l + OBJECT_BBOX_WIDTH;
+	b = t + OBJECT_BBOX_HEIGHT;
+}
